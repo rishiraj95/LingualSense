6d06f6babba516079cb0eae339be4251d312dfe0
VaibhaviCNBhat <vaibhavibhat3007@gmail.com>
2024-11-14 19:08:00 +0530

    Solution to problem 1 and 2

--- assignment_4_11_24/solution2.py
+++ assignment_4_11_24/solution2.py
@@ -1,34 +1,19 @@
-def min_to_max_operations(arr):
-    """
-    Calculates the minimum number of operations to make the minimum value the maximum.
+t = int(input())
+answers = []
 
-    Args:
-        arr: The input array.
-
-    Returns:
-        The minimum number of operations.
-    """
-
-    min_val = min(arr)
-    max_val = max(arr)
-
-    # If the minimum is already the maximum, no operations are needed.
-    if min_val == max_val:
-        return 0
-    # Count elements greater than the minimum value
-    operations = 0
-    for num in arr:
-        if num > min_val:
-            operations += 1
-
-    return operations
-
-results = []
-T = int(input())
-for _ in range(T):
-    N = int(input())
+for _ in range(t):
+    n = int(input())
     arr = list(map(int, input().split()))
-    results.append(min_to_max_operations(arr))
     
-for result in results:
-    print(result)
+    min_val =min(arr)
+    max_val =max(arr)
+    ops =0
+    
+    if min_val != max_val:
+        for num in arr:
+            if num > min_val:
+                ops += 1
+    
+    answers.append(str(ops))
+
+print("\n".join(answers))
